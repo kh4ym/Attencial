71a7635ab05b183331de75f02439eb4d8fb146ad
khayyam <khayyamm15@gmail.com>
2026-05-09 23:32:28 +0500

    Add project files.

---
+++ Attencial.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Attencial.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Attencial.Shared/Constants/ApiEndpoints.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attencial.Shared.Constants
+{
+    internal class ApiEndpoints
+    {
+    }
+}
