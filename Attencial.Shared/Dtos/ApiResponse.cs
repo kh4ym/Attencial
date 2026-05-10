c11a5cd9658e7b6da67f16ac3efc519e8d3249f1
khayyam <khayyamm15@gmail.com>
2026-05-10 13:48:57 +0500

    project structure resolved. project type and reference issues fixed.

--- Attencial.Shared/Dtos/ApiResponse.cs
+++ Attencial.Shared/Dtos/ApiResponse.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+
 
 namespace Attencial.Shared.Dtos
 {
-    internal class ApiResponse
+    public class ApiResponse
     {
+
     }
 }
