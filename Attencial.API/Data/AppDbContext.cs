6722a6b98934ace9253bb39e76117cd23f800c0b
khayyam <khayyamm15@gmail.com>
2026-05-11 19:25:54 +0500

    group members added as collaborators and seperate branches made

---
+++ Attencial.API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Attencial.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Attencial.API.Datal;
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
