b6367c011cf21137c0af0513e8b864a51c6c0bcc
khayyam <khayyamm15@gmail.com>
2026-05-13 22:34:45 +0500

    jwt issues fixed

--- Attencial.Shared/Dtos/LoginRequest.cs
+++ Attencial.Shared/Dtos/LoginRequest.cs
@@ -4,9 +4,3 @@ using System.Text;
 
 namespace Attencial.Shared.Dtos;
 
-public record LoginRequest
-{
-    public string Email { get; init; } = string.Empty;
-    public string Password { get; init; } = string.Empty;
-    public string Role { get; init; } = string.Empty;
-}
